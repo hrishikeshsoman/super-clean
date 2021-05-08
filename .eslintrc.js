6b7ae8741759a23ad6b81f46604620e2644241c4
hrishikeshsoman3 <hrishikeshsoman3@gmail.com>
2021-05-08 08:41:40 +0530

    Eslint issues

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    parser: "babel-eslint",
+    extends: "react",
+    env: {
+        browser: true,
+        node: true
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
+    }
+};
